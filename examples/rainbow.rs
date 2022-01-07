b12f2187e4772ecfd75daf8e0097b22edc2d7cba
Davis Schenkenberger <davisschenk@gmail.com>
2022-01-06 20:26:07 -0700

    Add rainbow and color wheel

---
+++ examples/rainbow.rs
@@ -0,0 +1,19 @@
+use rasp_leds_v2::{Controller, Pattern, RainbowCycle};
+use std::{thread::sleep, time::Duration};
+
+fn main() {
+    #[cfg(feature = "simulate")]
+    let mut c = Controller::new(150, 25);
+
+    #[cfg(feature = "hardware")]
+    let mut c = Controller::new(150, 18, 255);
+
+    let mut pattern = RainbowCycle::new(100);
+
+    pattern.init(&mut c).unwrap();
+
+    for i in 0..u64::MAX {
+        pattern.start_tick(i, &mut c).unwrap();
+        sleep(Duration::from_millis(pattern.tick_rate()))
+    }
+}
---
+++ src/patterns/rainbow.rs
@@ -0,0 +1,38 @@
+use crate::controller::{Controller, LedController};
+use crate::patterns::Pattern;
+use anyhow::Result;
+
+pub struct RainbowCycle {
+    pub tick_rate: u64,
+}
+
+impl RainbowCycle {
+    pub fn new(tick_rate: u64) -> RainbowCycle {
+        RainbowCycle { tick_rate }
+    }
+}
+
+impl Pattern for RainbowCycle {
+    fn tick_rate(&self) -> u64 {
+        self.tick_rate
+    }
+
+    fn tick_cycle(&self) -> Option<u64> {
+        Some(256 * 5)
+    }
+
+    fn tick(&mut self, tick: u64, controller: &mut Controller) -> Result<()> {
+        let count = controller.get_count();
+        let leds = controller.get_data();
+
+        for i in 0..leds.len() {
+            leds[i] = crate::color::wheel(
+                (((i as f64 * 256.0 / count as f64) as u64 + tick) & 255) as u8,
+            );
+        }
+
+        let _ = controller.update();
+
+        Ok(())
+    }
+}
