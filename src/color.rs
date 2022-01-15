18e5b299e2df11ab3e6008420b584c64fbb7a5b3
Davis Schenkenberger <davisschenk@gmail.com>
2022-01-14 21:07:44 -0700

    Fix fader

--- src/color.rs
+++ src/color.rs
@@ -17,22 +17,23 @@ impl Color {
     }
 
     pub fn fade_to_black(&self, amt: u8) -> Color {
+        let frac: f64 = amt as f64 / 255.0;
         match self {
             Color::RGB(r, g, b) => Color::RGB(
                 if *r <= 10 {
                     0
                 } else {
-                    *r - (*r as f64 * amt as f64 / 255.0) as u8
-                },
-                if *b <= 10 {
-                    0
-                } else {
-                    *b - (*b as f64 * amt as f64 / 255.0) as u8
+                    *r - (*r as f64 * frac) as u8
                 },
                 if *g <= 10 {
                     0
                 } else {
-                    *g - (*g as f64 * amt as f64 / 255.0) as u8
+                    *g - (*g as f64 * frac) as u8
+                },
+                if *b <= 10 {
+                    0
+                } else {
+                    *b - (*b as f64 * frac) as u8
                 },
             ),
         }
